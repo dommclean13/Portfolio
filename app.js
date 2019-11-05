4c8af7d675c9a17af20d9d22baef5958fa34b9be
Dominique McLean <dommclean13@gmail.com>
2019-11-04 18:42:27 -0600

    editing index.html page

---
+++ app.js
@@ -0,0 +1,8 @@
+// $('.navicon').on('click', function (e) {
+//     e.preventDefault();
+//     $(this).toggleClass('navicon--active');
+//     $('.toggle').toggleClass('toggle--active');
+//   });
+
+
+
